e2c146da92bcf34a0d70b55e113bb75411c267c5
Adriana Vargas Villalobos <avargasvi@ucenfotec.ac.cr>
2025-04-24 21:02:37 -0600

    [Update] Multiplica

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,10 +3,13 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-from src.operations import suma, resta
+from src.operations import suma, resta, multiplica
 
 def test_suma():
     assert suma(2, 3) == 5
 
 def test_resta():
     assert resta(5, 3) == 2
+
+def test_mulltiplica():
+    assert multiplica(6, 3) == 18
